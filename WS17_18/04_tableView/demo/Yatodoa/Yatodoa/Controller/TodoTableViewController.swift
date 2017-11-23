44198b6035431d43500f96c524e5890560d0f88f
Alexander Dobrynin <info@alex-dobrynin.com>
2017-11-23 10:27:35 +0100

    04 slides, demo and assignment

---
+++ WS17_18/04_tableView/demo/Yatodoa/Yatodoa/Controller/TodoTableViewController.swift
@@ -0,0 +1,140 @@
+//
+//  TodoTableViewController.swift
+//  Yatodoa
+//
+//  Created by Alex on 21.11.17.
+//  Copyright © 2017 Alexander Dobrynin. All rights reserved.
+//
+
+import UIKit
+
+enum TodoType {
+    case add, open
+}
+
+extension TodoType {
+    
+    init(todo: ToDo) {
+        self = todo.task.isEmpty ? .add : .open
+    }
+    
+    var title: String {
+        switch self {
+        case .add: return "Neu Hinzufügen"
+        case .open: return "offen"
+        }
+    }
+    
+    static func < (lhs: TodoType, rhs: TodoType) -> Bool {
+        return lhs.hashValue < rhs.hashValue
+    }
+}
+
+class TodoTableViewController: UITableViewController {
+
+    var todos: [ToDo] = [] {
+        didSet {
+            print(todos)
+            
+            todoSections = todos.groupBy(key: { todo in
+                return TodoType(todo: todo)
+            }).sorted(by: { (l, r) in
+                return l.key < r.key
+            })
+        }
+    }
+    
+    var todoSections: [(key: TodoType, value: [ToDo])] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UINib(nibName: TodoTableViewCell.NibName, bundle: nil), forCellReuseIdentifier: TodoTableViewCell.ReuseIdentifier)
+        tableView.register(UINib(nibName: AddTodoTableViewCell.NibName, bundle: nil), forCellReuseIdentifier: AddTodoTableViewCell.ReuseIdentifier)
+
+        todos.append(ToDo.empty)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return todoSections.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoSections[section].value.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch todoSections[indexPath.section].key {
+        case .add:
+            let cell = tableView.dequeueReusableCell(withIdentifier: AddTodoTableViewCell.ReuseIdentifier, for: indexPath) as! AddTodoTableViewCell
+            
+            cell.delegate = self
+            
+            return cell
+        case .open:
+            let cell = tableView.dequeueReusableCell(withIdentifier: TodoTableViewCell.ReuseIdentifier, for: indexPath) as! TodoTableViewCell
+            
+            let todo = todoSections[indexPath.section].value[indexPath.row]
+            cell.todo = todo.task
+            cell.delegate = self
+            
+            return cell
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return todoSections[section].key.title
+    }
+
+    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    private func insert(_ todo: ToDo, animated: Bool) {
+        if animated {
+            let section = TodoType.open
+            let sectionExists = todoSections.contains(where: { entry in entry.key == section })
+            todos.append(todo)
+            
+            let nextPositon = todoSections.first(where: { entry in entry.key == section })!.value.count - 1
+            
+            let indexPath = IndexPath(row: nextPositon, section: section.hashValue)
+            
+            if sectionExists {
+                tableView.insertRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+            } else {
+                tableView.beginUpdates()
+                tableView.insertSections(IndexSet(integer: section.hashValue), with: .automatic)
+                tableView.insertRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+                tableView.endUpdates()
+            }
+            
+            
+        } else {
+            todos.append(todo) // update model
+            tableView.reloadData()
+        }
+    }
+}
+
+extension TodoTableViewController: AddTodoTableViewCellDelegate {
+    func addTodoCell(_ cell: AddTodoTableViewCell, didCreateTodo todo: String) {
+        insert(ToDo(task: todo), animated: true)
+        
+        cell.textField.text = nil
+    }
+}
+
+extension TodoTableViewController: TodoTableViewCellDelegate {
+    func todoCell(_ cell: TodoTableViewCell, wasCompleted completed: Bool) {
+        let indexPath = tableView.indexPath(for: cell)!
+        let todo = todoSections[indexPath.section].value[indexPath.row]
+        
+        if let index = todos.index(of: todo) {
+            todos[index] = ToDo(task: todo.task, completed: completed)
+            tableView.reloadData()
+        }
+    }
+}
