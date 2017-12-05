3cd6ccc5a4b9637e19c91570b96e05ca38aaf3e4
Alexander Dobrynin <info@alex-dobrynin.com>
2017-12-05 17:05:57 +0100

    slides, demo and assignment for 06

---
+++ WS17_18/06_design/demo/Yatodoa/Yatodoa/Controller/NotesViewController.swift
@@ -0,0 +1,63 @@
+//
+//  NotesViewController.swift
+//  Yatodoa
+//
+//  Created by Alex on 28.11.17.
+//  Copyright © 2017 Alexander Dobrynin. All rights reserved.
+//
+
+import UIKit
+
+class NotesViewController: UIViewController {
+    
+    struct Storyboard {
+        static let TextViewPlaceholder = "Notiz..."
+        static let UnwindSegueIdentifier = "NotesUnwindSegueIdentifier"
+    }
+
+    var notes: String?
+    
+    @IBOutlet weak var notesTextView: UITextView! {
+        didSet { notesTextView.delegate = self }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        notesTextView.text = notes
+    }
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func done(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: Storyboard.UnwindSegueIdentifier, sender: notesTextView.text)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier else { return }
+        
+        switch identifier {
+        case Storyboard.UnwindSegueIdentifier:
+            notes = sender as? String
+            
+        default: break
+        }
+    }
+}
+
+extension NotesViewController: UITextViewDelegate {
+    
+     func textViewDidBeginEditing(_ textView: UITextView) {
+         if textView.text == Storyboard.TextViewPlaceholder {
+            textView.text = ""
+         }
+     }
+    
+     func textViewDidEndEditing(_ textView: UITextView) {
+         if textView.text.isEmpty {
+            textView.text = Storyboard.TextViewPlaceholder
+         }
+     }
+ }
